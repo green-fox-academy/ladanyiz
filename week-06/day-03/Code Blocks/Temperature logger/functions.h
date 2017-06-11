566b218d3f4682294a67b75ac7fd639940f7a7f8
ladanyiz <ladzoltan@gmail.com>
2017-06-11 19:01:23 +0200

    first half of date error checking

--- week-06/day-03/Code Blocks/Temperature logger/functions.h
+++ week-06/day-03/Code Blocks/Temperature logger/functions.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
+#define BAUD_RATE   115200
+
 typedef struct {
     int8_t temp_array[1000];
     uint8_t array_size;
@@ -14,31 +16,33 @@ char time_buffer[30];
 
 char file_name[20];
 char port_name[5];
-uint8_t index;
+uint8_t port_index;
 
 
 void start_screen();
 
 void list_ports();
 
-int8_t set_port();
+void set_port();
 
 int8_t file_ok(const char *filename, const char *op);
 
 void file_to_write();
 
-int8_t open_port();
+void open_port();
 
 char *timestamp();
 
-int8_t write_file(char *filename);
+int8_t write_file(const char *filename);
 
 void close_port();
 
 void file_to_read();
 
-int8_t read_file(const char *filename, const char *from, const char *to, list_type *t_list);
+void read_file(const char *filename, const char *from, const char *to, list_type *t_list);
 
 void calc_avg();
+
+int8_t date_ok(char *string);
 
 #endif // FUNCTIONS_H_INCLUDED
