eb097df2bf09b6e187091fe8a2e03fc7eee15a84
ladanyiz <ladzoltan@gmail.com>
2017-06-27 11:49:44 +0200

    basic logging added

--- week-09/day-01/Code Blocks/02_Create server/main.c
+++ week-09/day-01/Code Blocks/02_Create server/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <winsock2.h>
-#include "file_IO.h"
+#include "timestamp.h"
 
 #define PORT 777
 
@@ -12,7 +12,7 @@
 int main()
 {
     WSADATA wsa_data;
-    char *path = "log.txt";
+    FILE *file = fopen("log.txt", "a");
 
     // Initialize Winsock version 2.2
     printf("\nInitializing Winsock...");
@@ -22,6 +22,7 @@ int main()
         return 1;
     }
     printf("Initialized.\n");
+    fprintf(file, "%s\tWinsock initialized\n", timestamp());
 
     // Create a socket
     SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
@@ -30,6 +31,7 @@ int main()
         return 1;
     }
     printf("Socket created.\n");
+    fprintf(file, "%s\tSocket created\n", timestamp());
 
     //Prepare the sockaddr_in structure
     SOCKADDR_IN server;
@@ -41,12 +43,14 @@ int main()
     if( bind(listening ,(SOCKADDR*)&server , sizeof(server)) == SOCKET_ERROR)
         printf("Bind failed with error code %d", WSAGetLastError());
     puts("Bind done.");
+    fprintf(file, "%s\tBind done\n", timestamp());
 
     // Tell WinSock the socket is for listening
     listen(listening, SOMAXCONN);
 
     // Wait for a connection
     puts("Waiting for incoming connections...");
+    fprintf(file, "%s\tWaiting for incoming connections...\n", timestamp());
 
     SOCKADDR_IN client;
     int client_size = sizeof(client);
@@ -58,23 +62,32 @@ int main()
         printf("Accept failed with error code : %d" , WSAGetLastError());
         return 1;
     }
-
-    puts("Connection accepted");
-    file_write_line_logging_mode(path, "Connection accepted");
+    char *client_ip = inet_ntoa(client.sin_addr);
+    int client_port = ntohs(client.sin_port);
+    printf("Connection accepted from %s on port %d\n", client_ip, client_port);
+    fprintf(file, "%s\tConnection accepted from %s on port %d\n", timestamp(), client_ip, client_port);
 
     // Communicating with client
     char buffer[100];
     int received = 0;
     while (1) {
         received = recv(client_socket, buffer, 100, 0);
+        if(received == SOCKET_ERROR) {
+            puts("recv failed");
+            return 1;
+        }
         // Put a terminator at the and of the buffer to create a proper string
         buffer[received] = '\0';
         // Print out message just for the show ;)
         puts(buffer);
+        fprintf(file, "%s\tReceived :%s\n", timestamp(), buffer);
+
         send(client_socket , buffer , received , 0 );
+        fprintf(file, "%s\tSent :%s\n", timestamp(), buffer);
     }
 
     closesocket(listening);
+    fprintf(file, "%s\tSocket closed\n", timestamp(), buffer);
 
     // When your application is finished call WSACleanup
     if (WSACleanup() == SOCKET_ERROR)
---
+++ week-09/day-01/Code Blocks/02_Create server/timestamp.h
@@ -0,0 +1,6 @@
+#ifndef TIMESTAMP_H_INCLUDED
+#define TIMESTAMP_H_INCLUDED
+
+char *timestamp();
+
+#endif // TIMESTAMP_H_INCLUDED
